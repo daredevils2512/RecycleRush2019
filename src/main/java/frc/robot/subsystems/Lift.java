9ad5d8140c1f4343997b0eff0ea8893b627d4c40
Toby <tobykothe@gmail.com>
2019-04-22 18:11:32 -0500

    worked on updating recycle rush things

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -1,12 +1,14 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANPIDController;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Robot;
 import frc.robot.subsystems.*;
@@ -20,14 +22,17 @@ public class Lift extends Subsystem {
     private DigitalInput liftBottom;
     private DigitalInput liftTop;
 
-    private TalonSRX lift1;
-    private TalonSRX lift2;
+    private WPI_TalonSRX lift1;
+    private WPI_TalonSRX lift2;
 
     private Encoder heightEncoder;
 
     public void Lift() {
         liftBottom = new DigitalInput(11);
         liftTop = new DigitalInput(10);
+
+        lift1 = new WPI_TalonSRX(8);
+        lift2 = new WPI_TalonSRX(2);
 
         heightEncoder = new Encoder(8, 9, false);
         heightEncoder.setDistancePerPulse(1);
@@ -41,23 +46,48 @@ public class Lift extends Subsystem {
     public void usePIDOutput(double output) {
         if ((!liftBottom.get() || (output < 0 && Robot.m_intake.solenoidGet() == Value.kForward))
          && ((!liftTop.get() || Robot.m_oi.extreme.getRawButton(7)) || output > 0 )) {
-            float motorSeting;
+            float motorSetting;
             if (output > 0.05) {
                 if (output >= 0.1) {
-                    motorSeting = (float) output;
+                    motorSetting = (float) output;
                 } else {
-                    motorSeting = (float) 0.1;
+                    motorSetting = (float) 0.1;
                 }
             } else if (output < -0.05) {
                 if (output <= -0.1) {
-                    motorSeting = (float) output;
+                    motorSetting = (float) output;
                 } else {
-                    motorSeting = (float) -0.1;
+                    motorSetting = (float) -0.1;
                 }
             } else {
-                motorSeting = 0;
+                motorSetting = 0;
             }
+            lift1.pidWrite(-motorSetting);
+            lift2.pidWrite(motorSetting);
+        } else {
+            lift1.set(0);
+            lift2.set(0);
+        }
+    }
 
+    public void setMotor(float velocity) {
+        if (!liftBottom.get() && ((!liftTop.get() || Robot.m_oi.extreme.getRawButton(7)) || velocity < 0)) {
+            if (heightEncoder.getRaw() >= -2 && Robot.m_intake.solenoidGet() == Value.kReverse) {
+                lift1.set(velocity);
+                lift2.set(-velocity);
+            } else {
+                lift1.set(0);
+                lift2.set(0);
+            }
+        } else {
+            if ((velocity > 0 && Robot.m_intake.solenoidGet() == Value.kForward) && ((!liftTop.get() ||
+             Robot.m_oi.extreme.getRawButton(7)) || velocity < 0)) {
+                 lift1.set(velocity);
+                 lift1.set(-velocity);
+             } else {
+                 lift1.set(0);
+                 lift2.set(0);
+             }
         }
     }
 }
